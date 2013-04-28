9461c50ee28884a22753554d15d7877fb14309cd
Michal Partyka <partyks@gmail.com>
2013-04-28 15:25:17 +0200

    DiceSpecificStatistics

--- src/main/java/pl/edu/agh/to1/dice/StatisticsModel/DiceSpecifiedStatistics.java
+++ src/main/java/pl/edu/agh/to1/dice/StatisticsModel/DiceSpecifiedStatistics.java
@@ -9,4 +9,35 @@ import javax.persistence.Entity;
 public class DiceSpecifiedStatistics extends SpecifiedStatistics {
     private Integer sumOfPoints;
     private Integer sumOfBonusPoints;
+
+    public DiceSpecifiedStatistics() {
+    }
+
+    public DiceSpecifiedStatistics(Integer sumOfPoints, Integer sumOfBonusPoints) {
+        this.sumOfPoints = sumOfPoints;
+        this.sumOfBonusPoints = sumOfBonusPoints;
+    }
+
+    public DiceSpecifiedStatistics(GameInfo gameInfo, Integer amountOfPlayedGames, Integer amountOfWinGames,
+                                   Integer amountOfEscapes, Integer sumOfPoints, Integer sumOfBonusPoints) {
+        super(gameInfo, amountOfPlayedGames, amountOfWinGames, amountOfEscapes);
+        this.sumOfPoints = sumOfPoints;
+        this.sumOfBonusPoints = sumOfBonusPoints;
+    }
+
+    public Integer getSumOfPoints() {
+        return sumOfPoints;
+    }
+
+    public void setSumOfPoints(Integer sumOfPoints) {
+        this.sumOfPoints = sumOfPoints;
+    }
+
+    public Integer getSumOfBonusPoints() {
+        return sumOfBonusPoints;
+    }
+
+    public void setSumOfBonusPoints(Integer sumOfBonusPoints) {
+        this.sumOfBonusPoints = sumOfBonusPoints;
+    }
 }
