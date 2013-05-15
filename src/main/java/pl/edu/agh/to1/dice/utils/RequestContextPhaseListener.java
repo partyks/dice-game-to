7430a5653cd9a154cbd731897190db1b0fda0859
Michal Partyka <partyks@gmail.com>
2013-05-15 13:41:49 +0200

    phaseListener

--- src/main/java/pl/edu/agh/to1/dice/utils/RequestContextPhaseListener.java
+++ src/main/java/pl/edu/agh/to1/dice/utils/RequestContextPhaseListener.java
@@ -1,7 +1,35 @@
 package pl.edu.agh.to1.dice.utils;
 
+import org.primefaces.context.DefaultRequestContext;
+import org.primefaces.util.Constants;
+
+import javax.faces.context.FacesContext;
+import javax.faces.event.PhaseEvent;
+import javax.faces.event.PhaseId;
+import javax.faces.event.PhaseListener;
+
 /**
  * Author: Piotr Turek
  */
-public class RequestContextPhaseListener {
+public class RequestContextPhaseListener implements PhaseListener {
+    private static final long serialVersionUID = 1832618590862971753L;
+
+    @Override
+    public void afterPhase(PhaseEvent event) {
+
+    }
+
+    @Override
+    public void beforePhase(PhaseEvent event) {
+        FacesContext context = event.getFacesContext();
+        if (context.getAttributes().get(Constants.REQUEST_CONTEXT_ATTR) == null) {
+            context.getAttributes().put(Constants.REQUEST_CONTEXT_ATTR, new DefaultRequestContext());
+        }
+    }
+
+    @Override
+    public PhaseId getPhaseId() {
+        return PhaseId.RENDER_RESPONSE;
+    }
+
 }
